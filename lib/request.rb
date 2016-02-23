605eb5c622454968a180d251de06846b17026c46
Ashwin Rao <theonlyrao@gmail.com>
2016-02-22 21:22:44 -0700

    started request class

---
+++ lib/request.rb
@@ -0,0 +1,14 @@
+class Request
+
+  def initialize(incoming_request)
+    @incoming_request = incoming_request
+  end
+
+  def incoming_to_hash
+    request_hash = Hash.new
+    @incoming_request.each do |info|
+      
+    end
+  end
+
+end
