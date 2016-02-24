6fe54d756069d19606ab3e4dc472a59b82b02704
Ashwin Rao <theonlyrao@gmail.com>
2016-02-24 15:10:55 -0700

    got everything back to where it should be after wierdness

--- lib/http.rb
+++ lib/http.rb
@@ -3,29 +3,27 @@ require 'pry'
 require_relative 'request'
 
 tcp_server = TCPServer.new(9292)
-counter = 0
+
+hello_counter = 0
+total_counter = 0
 
 loop do
   client = tcp_server.accept
 
-  puts "Ready for a request"
   request_lines = []
   while line = client.gets and !line.chomp.empty?
     request_lines << line.chomp
   end
-  request = Request.new(request_lines)
-  request.incoming_to_hash
 
-  counter +=1
+  request = Request.new(request_lines)
+
+  puts "Ready for a request"
+  total_counter += 1
 
   puts "Got this request:"
-  puts request_lines.join("\n") # should be something called on request
-
 
   puts "Sending response."
-  # all of this should be moved to request class
-
-  response = "<pre>Hello World! (#{counter})</pre>"
+  response = "<pre>Hello World! #{total_counter}</pre>"
   output = "<html><head></head><body>#{response}</body></html>"
   headers = ["http/1.1 200 ok",
             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
@@ -34,8 +32,12 @@ loop do
             "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   client.puts headers
   client.puts output
+
+  binding.pry
+
+  puts "#{total_counter}"
+
 end
 
-puts ["Wrote this response:", headers, output].join("\n")
 client.close
 puts "\nResponse complete, exiting."
--- lib/request.rb
+++ lib/request.rb
@@ -2,26 +2,19 @@ require 'pry'
 
 class Request
 
-  def initialize(incoming_request)
-    @incoming_request = incoming_request
+  attr_accessor :request_hash
+
+  def initialize(request_lines)
+    request_to_hash(request_lines)
   end
 
-  def incoming_to_hash
+  def request_to_hash(request_lines)
     @request_hash = Hash.new
-    @request_hash[:Verb] = @incoming_request.first.split[0]
-    @request_hash[:Protocol] = @incoming_request.first.split[2]
-    @request_hash[:Host] = @incoming_request[1].split[1].split(":").first
-    @request_hash[:Path] = @incoming_request.first.split[1]
-    @request_hash[:Port] = @incoming_request[1].split[1].split(":").last
+    @request_hash[:Verb] = request_lines.first.split[0]
+    @request_hash[:Protocol] = request_lines.first.split[2]
+    @request_hash[:Host] = request_lines[1].split[1].split(":").first
+    @request_hash[:Path] = request_lines.first.split[1]
+    @request_hash[:Port] = request_lines[1].split[1].split(":").last
   end
-
-  def response
-    "Hello World! (COUNTER GOES HERE)"
-  end
-
-  def request_hash
-    @request_hash
-  end
-
 
 end
